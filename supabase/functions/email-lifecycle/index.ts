2b2b7f28c742ef711af89adc7a5e6e8f6e98820e
rudivanek <rfv@datago.net>
2026-05-29 13:00:10 -0600

    Updated index.ts

--- supabase/functions/email-lifecycle/index.ts
+++ supabase/functions/email-lifecycle/index.ts
@@ -3,7 +3,7 @@ import { createClient } from "npm:@supabase/supabase-js@2";
 import { runScheduler } from "./scheduler.ts";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://tu-animo.app",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
 };
